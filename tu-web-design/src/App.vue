8987ddcbd7e947a4ec8f9bf6b2cfa5aa03c19658
zahari-ninov <133575840+HarkataBG@users.noreply.github.com>
2024-12-15 15:18:10 +0200

    Add project structure

---
+++ tu-web-design/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <header>
+      <h1>Achivy - Add your new post here!</h1>
+    </header>
+    <AddProjectSubject @addProjectSubject="addSubject" />
+    <ProjectSubjectFilter @filterUpdated="filterSubjects" />
+    <ProjectSubjectList
+      :subjects="filteredSubjects"
+      @deleteSubject="deleteSubject"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+import AddProjectSubject from './components/AddProjectSubject.vue';
+import ProjectSubjectList from './components/ProjectSubjectList.vue';
+import ProjectSubjectFilter from './components/ProjectSubjectFilter.vue';
+
+const subjects = ref([]);
+const filterText = ref('');
+
+function addSubject(newSubject) {
+  subjects.value.push(newSubject);
+}
+
+function deleteSubject(index) {
+  subjects.value.splice(index, 1);
+}
+
+function filterSubjects(filter) {
+  filterText.value = filter;
+}
+
+const filteredSubjects = computed(() =>
+  subjects.value.filter((subject) =>
+    subject.name.toLowerCase().includes(filterText.value.toLowerCase())
+  )
+);
+</script>
